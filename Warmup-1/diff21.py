b4c44d627b699c94ab1a1945627a463e50bc657c
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 10:51:40 -0600

    description

--- Warmup-1/diff21.py
+++ Warmup-1/diff21.py
@@ -20,5 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given an int n, return the absolute difference between n and 21, except return double the
+# absolute difference if n is over 21.
+#
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+
 def diff21(n):
     return 2 * (n - 21) if n > 21 else 21 - n;
