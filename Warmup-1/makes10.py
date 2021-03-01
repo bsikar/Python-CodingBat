8ec6fa4fbd64b918f56cb1f4d7fccec20585c8b2
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 10:54:24 -0600

    description

--- Warmup-1/makes10.py
+++ Warmup-1/makes10.py
@@ -20,5 +20,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+#
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
 def makes10(a, b):
     return ((a + b == 10) or (a == 10) or (b == 10));
