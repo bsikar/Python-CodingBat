965632290ef84694bbdfc0d14fcf7ab40738dce9
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 11:00:13 -0600

    description

--- Warmup-1/not_string.py
+++ Warmup-1/not_string.py
@@ -20,6 +20,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given a string, return a new string where "not " has been added to the front. However, if
+# the string already begins with "not", return the string unchanged.
+#
+# not_string('candy') → 'not candy'
+# not_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
 def not_string(str):
     if len(str) >= 3 and str[:3] == "not":
         return str
