720c3494c7e8821505b59939b596dfc9bbf9eb7d
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 11:06:41 -0600

    description

--- Warmup-1/front3.py
+++ Warmup-1/front3.py
@@ -20,6 +20,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given a string, we'll say that the front is the first 3 chars of the string. If the string length
+# is less than 3, the front is whatever is there. Return a new string which is 3 copies of the
+# front.
+#
+# front3('Java') → 'JavJavJav'
+# front3('Chocolate') → 'ChoChoCho'
+# front3('abc') → 'abcabcabc'
+
 def front3(str):
     x = str;
     if len(str) >= 3:
