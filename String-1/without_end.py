32d011f78384d1eed5d686c732c109b5b9698e04
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:40:20 -0600

    description

--- String-1/without_end.py
+++ String-1/without_end.py
@@ -20,5 +20,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will
+# be at least 2.
+# 
+# without_end('Hello') → 'ell'
+# without_end('java') → 'av'
+# without_end('coding') → 'odin'
+
 def without_end(str):
     return str[1:-1]
