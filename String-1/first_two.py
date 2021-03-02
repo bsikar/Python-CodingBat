0c832d9fd83fb7a6b18d437351a4b525da5e7326
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:37:58 -0600

    description

--- String-1/first_two.py
+++ String-1/first_two.py
@@ -20,6 +20,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given a string, return the string made of its first two chars, so the String "Hello" yields "He". If the string
+# is shorter than length 2, return whatever there is, so "X" yields "X", and the empty string "" yields the
+# empty string "".
+# 
+# first_two('Hello') → 'He'
+# first_two('abcdefg') → 'ab'
+# first_two('ab') → 'ab'
+
 def first_two(str):
     if len(str) >= 2:
         return str[:2]
