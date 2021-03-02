dcea6aef340f3676f2e571ef0c0c7346510bf11d
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:39:01 -0600

    description

--- String-1/first_half.py
+++ String-1/first_half.py
@@ -20,5 +20,11 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+# 
+# first_half('WooHoo') → 'Woo'
+# first_half('HelloThere') → 'Hello' 
+# first_half('abcdef') → 'abc'
+
 def first_half(str):
     return str[:len(str)/2]
